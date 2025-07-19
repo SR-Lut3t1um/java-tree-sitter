22f40f6b59538058b77dacdd6c559e2d677724af
tobias <mail@tobiasliese.me>
2025-07-19 23:31:37 +0200

    added missing methods in global attributes

--- src/main/java/me/tobiasliese/treesitter/jx/jx_parser/parser/html/H1ElementParser.java
+++ src/main/java/me/tobiasliese/treesitter/jx/jx_parser/parser/html/H1ElementParser.java
@@ -3,6 +3,7 @@ package me.tobiasliese.treesitter.jx.jx_parser.parser.html;
 import io.github.treesitter.jtreesitter.Node;
 import me.tobiasliese.treesitter.jx.jx_parser.result.html.GlobalAttributes;
 import me.tobiasliese.treesitter.jx.jx_parser.result.html.H1Element;
+import me.tobiasliese.treesitter.jx.jx_parser.result.html.builder.GlobalAttributesBuilder;
 
 public class H1ElementParser {
     public static H1Element parseH1Element(Node node) {
@@ -17,12 +18,12 @@ public class H1ElementParser {
     }
 
     private static void parseH1Attributes(Node node) {
-        GlobalAttributes attributes = new GlobalAttributes();
+        GlobalAttributesBuilder builder = new GlobalAttributesBuilder();
         for (var child : node.getChildren()) {
             System.out.println(child.getType());
             switch (child.getType()) {
                 case "html_global_attribute" -> {
-                    HtmlElementParser.parseGlobalAttribute(child, attributes);
+                    HtmlElementParser.parseGlobalAttribute(child, builder);
                 }
             }
         }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module jtreesitter {
     requires org.jspecify;
+    uses io.github.treesitter.jtreesitter.NativeLibraryLookup;
     exports me.tobiasliese.treesitter.jx.jx_parser.result.module;
     exports me.tobiasliese.treesitter.jx.jx_parser.result.html;
     exports me.tobiasliese.treesitter.jx.jx_parser.result.html.builder;
